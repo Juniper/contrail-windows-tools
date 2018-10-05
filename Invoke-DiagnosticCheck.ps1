695435bf17eef16900aab9cf9b767c7f5f1c0f21
Dariusz Sosnowski <sodar@users.noreply.github.com>
2018-10-05 16:42:27 +0200

    Add a note regarding npcap

--- Invoke-DiagnosticCheck.ps1
+++ Invoke-DiagnosticCheck.ps1
@@ -159,6 +159,10 @@ Describe "Diagnostic check" {
         }
 
         It "npcap should not be installed" {
+            # npcap (and its kernel drivers) interfere with the way Windows TCP/IP stack
+            # works. For example, it disables checksum offloading on container interfaces.
+            # To provide a consistent behaviour on test and dev environments, npcap
+            # should be uninstalled.
             { Get-Service npcap } | Should Throw
         }
     }

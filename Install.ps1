1a2284d6d1579f45d2fbbe3a103064c96ef5dad6
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-12-05 14:39:51 +0100

    Add Install.ps1

---
+++ Install.ps1
@@ -0,0 +1,4 @@
+# This script is called automatically by Install-Module cmdlet, e.g. when:
+# Install-Module -ModuleUrl https://github.com/Juniper/contrail-windows-tools/raw/master/ContrailTools.psd1
+
+Install-Module .\ContrailTools.psm1

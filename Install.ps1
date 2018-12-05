8f273270d843543bcdda38ef26b8a86e5dc8ce35
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-12-05 14:56:56 +0100

    Fix typo.

--- Install.ps1
+++ Install.ps1
@@ -1,4 +1,5 @@
 # This script is called automatically by Install-Module cmdlet, e.g. when:
 # Install-Module -ModuleUrl https://github.com/Juniper/contrail-windows-tools/raw/master/ContrailTools.psd1
 
-Import-Module .\Contrail-Tools.psd1
+$InstallDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+Import-Module $InstallDir\ContrailTools.psd1

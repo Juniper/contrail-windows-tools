638efc646600a9edeee1f8241f519c18607b22c5
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-12-05 16:17:36 +0100

    Test if install is ran.

--- Install.ps1
+++ Install.ps1
@@ -1,12 +1,10 @@
 # This script is called automatically by Install-Module cmdlet, e.g. when:
 # Install-Module -ModuleUrl https://github.com/Juniper/contrail-windows-tools/archive/master.zip
 
-3/0
-
-# Install dependencies:
-Install-Module powershell-yaml
-
-
 $InstallDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 Import-Module $InstallDir\ContrailTools.psd1
+New-Item -ItemType File -Path $here\installed.txt | Out-Null
 
+Install-Module powershell-yaml
+
+New-Item -ItemType File -Path $here\after.txt | Out-Null

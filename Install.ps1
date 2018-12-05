0b5bcd0102f58df7961a48adb91f2ee8b02f7565
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-12-05 16:01:50 +0100

    Fixup install.ps1.

--- Install.ps1
+++ Install.ps1
@@ -1,8 +1,10 @@
 # This script is called automatically by Install-Module cmdlet, e.g. when:
-# Install-Module -ModuleUrl https://github.com/Juniper/contrail-windows-tools/raw/master/ContrailTools.psd1
+# Install-Module -ModuleUrl https://github.com/Juniper/contrail-windows-tools/archive/master.zip
+
+# Install dependencies:
+Install-Module powershell-yaml
+
 
 $InstallDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 Import-Module $InstallDir\ContrailTools.psd1
 
-# Install dependencies:
-Install-Module powershell-yaml

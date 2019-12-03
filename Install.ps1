f779de954ccf66b67913fa39a335759c331e6a60
sagarkchitnis <sagarc@juniper.net>
2019-12-03 11:13:26 -0800

    Windows is no longer supported (#29)

--- Install.ps1
+++
@@ -1,8 +0,0 @@
-# This script is called automatically by Install-Module cmdlet, e.g. when:
-# Install-Module -ModuleUrl https://github.com/Juniper/contrail-windows-tools/archive/master.zip
-
-$InstallDir = Split-Path $MyInvocation.MyCommand.Path -Parent
-Import-Module $InstallDir\ContrailTools.psd1
-
-# Install dependencies
-Install-Module -ModuleUrl "https://github.com/cloudbase/powershell-yaml/archive/master.zip"
